21cd643c171adaf0a75bc73d7755f72c75039c2b
sunilgurung123 <gurungpokhara007@yahoo.com>
2025-08-21 21:07:13 -0400

    updated CSS

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 let board= document.querySelector(".board");
 let resetButton = document.querySelector(".reset-btn");
+let gameOverMessage = document.getElementById("gameOverMessage");
 let foodX;
 let foodY;
 let snakeX;
@@ -27,6 +28,10 @@ function randomSnakePosition() {
 
 function moveSnake (event) {
     // console.log(event)
+    if (backgroundMusic.paused) {
+        backgroundMusic.play();
+    }
+
     if (event.key === "ArrowUp" && velocityY != 1){
         velocityX = 0;
         velocityY = -1;
@@ -51,42 +56,14 @@ function showGameOver () {
         document.removeEventListener("keydown", moveSnake)
         gameOverSound.play()
         backgroundMusic.pause(); 
-        document.getElementById("gameOverMessage").style.display = "block";
+        gameOverMessage.style.display = "block";
+        console.log(gameOverMessage)
 }
 //=====================
 
 //=======================
 
 function main() {
-    // if(gameOver) {
-    //     return showGameOver()
-    // }
-
-    // if (snakeX === foodX && snakeY === foodY) {
-    //     rendomFoodPosition()
-    //     eatFoodSound.play();
-    //     snakeBody.push([foodX,foodY])
-    //     console.log(snakeBody);
-    // }
-    // for (let i = snakeBody.length-1; i > 0; i-- ){
-    //     snakeBody[i] = snakeBody[i -1];
-    //     }
-    // let setHtml = `<div class="food" style="grid-area: ${foodY}/${foodX}";></div>`
-    // snakeX += velocityX;
-    // snakeY += velocityY;
-    // snakeBody[0] = [snakeX,snakeY]
-    // for (let i = 0; i < snakeBody.length; i++) {
-    //    setHtml += `<div class="snake-head" id="div${i}" style="grid-area: ${snakeBody[i][1]}/${snakeBody[i][0]}";></div>`
-    //    if(i!=0 && snakeBody[0][1]=== snakeBody[i][1] && snakeBody[0][0]=== snakeBody[i][0]) {
-    //     gameOver = true;
-    //    }
-    //     }
-
-    // if (snakeX<=0 || snakeX>20 || snakeY<=0 || snakeY >20) {
-    //     gameOver = true;
-    // }    
-    // board.innerHTML = setHtml;
-
     if (gameOver) {
         return showGameOver();
     }
@@ -149,8 +126,8 @@ function resetGame () {
     rendomFoodPosition();
     document.addEventListener("keydown", moveSnake);
     backgroundMusic.currentTime = 0;
-    backgroundMusic.play();
-    document.getElementById("gameOverMessage").style.display = "none";
+    // backgroundMusic.play();
+    gameOverMessage.style.display = "none";
     main();
     setIntervalId = setInterval(main, 300);
 }
@@ -158,7 +135,7 @@ function resetGame () {
 rendomFoodPosition()
 randomSnakePosition(); 
 snakeBody = [[snakeX, snakeY]];
-console.log("snake body:", snakeBody);
+// console.log("snake body:", snakeBody);
 main();
 setIntervalId = setInterval(main, 300);
 document.addEventListener("keydown", moveSnake);
